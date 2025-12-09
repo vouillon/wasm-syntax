cf8d5dd3eff35e85a48edc015acbde8e792e76cb
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-12-09 17:41:12 +0100

    -

--- src/bin/convert.ml
+++ src/bin/convert.ml
@@ -16,19 +16,21 @@ let convert ~filename =
   let ast'' =
     Utils.Diagnostic.run ~source:(Some source) (fun d -> Wax.Typing.f d ast')
   in
+  let ast3 = Wax.Typing.erase_types ast'' in
   let print_wax f m =
     Utils.Printer.run f (fun p -> Wax.Output.module_ ~out_channel:stdout p m)
   in
   Format.eprintf "%s==== %s ====%s@.@.%a@.@." Utils.Colors.Ansi.grey filename
-    Utils.Colors.Ansi.reset print_wax
-    (Wax.Typing.erase_types ast'');
-  let ast''' = Conversion.To_wasm.module_ ast'' in
+    Utils.Colors.Ansi.reset print_wax ast3;
+  let ast4 =
+    Utils.Diagnostic.run ~source:(Some source) (fun d -> Wax.Typing.f d ast3)
+  in
+  let ast5 = Conversion.To_wasm.module_ ast4 in
   let print_wasm f m =
     Utils.Printer.run f (fun p -> Wasm.Output.module_ ~out_channel:stdout p m)
   in
-  if false then Format.eprintf "%a@." print_wasm ast''';
-  Utils.Diagnostic.run ~source:(Some source) (fun d ->
-      Wasm.Validation.f d ast''')
+  if false then Format.eprintf "%a@." print_wasm ast5;
+  Utils.Diagnostic.run ~source:(Some source) (fun d -> Wasm.Validation.f d ast5)
 
 let _ =
   let p = "/home/jerome/wasm_of_ocaml/runtime/wasm" in
